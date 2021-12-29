b8e02b518901ada749ed44449337640f4ca6a842
CELS0 <celsojr.300@gmail.com>
2021-12-29 07:00:27 -0300

    feat

--- src/services/CreatePlaysService.ts
+++ src/services/CreatePlaysService.ts
@@ -16,6 +16,13 @@ class CreatePlaysService {
             throw new Error(`User ${name} already exists`);
         }
 
+        const limitPlays = await repository.find({bowl})
+        console.log(limitPlays)
+
+        if(limitPlays.length == 4) {
+            throw new Error(`Limit play in bowl`);
+        }
+
         const user = repository.create({ name, bowl, img: url });
 
         await repository.save(user)
